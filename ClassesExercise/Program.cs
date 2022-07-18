ecd07ea98a7fe0675b17ccdaece9d140044fde96
Jayden Callan <jqcallan04@gmail.com>
2022-07-18 12:35:27 -0600

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            car.Make = "Nissan";
+            car.Model = "Altima";
+            car.Year = 1993;
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
